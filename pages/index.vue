6a611da9a3976638875979e7909f1d283b7fbf43
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-11 00:53:19 +0100

    mocking api and using axios with nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
     <h1>Events</h1>
+    <event-card
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+
 export default {
+  components: {
+    EventCard
+  },
   head() {
     return {
       title: 'Event Listing'
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios.get('http://localhost:3001/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({ statusCode: 503, message: 'Unable to fetch events at this time. Please try again.' })
+      })
   }
 }
 </script>
