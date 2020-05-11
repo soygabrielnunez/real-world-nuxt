682c42b7c7b70cff22322bde6fc55e85ecc75b30
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-11 09:12:04 +0100

    refactor: extracting service

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService'
 
 export default {
   components: {
@@ -22,9 +23,9 @@ export default {
       title: 'Event Listing'
     }
   },
-  async asyncData({ $axios, error }) {
+  async asyncData({ error }) {
     try {
-      const { data } = await $axios.get('http://localhost:3001/events')
+      const { data } = await EventService.getEvents()
       return {
         events: data
       }
