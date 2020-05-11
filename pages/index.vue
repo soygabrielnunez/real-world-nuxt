204454d162bf02fd31fd14be8d4f8f56212dda87
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-11 01:05:58 +0100

    using async/await and the progress bar that nuxt offers

--- pages/index.vue
+++ pages/index.vue
@@ -22,16 +22,15 @@ export default {
       title: 'Event Listing'
     }
   },
-  asyncData({ $axios, error }) {
-    return $axios.get('http://localhost:3001/events')
-      .then(response => {
-        return {
-          events: response.data
-        }
-      })
-      .catch(e => {
-        error({ statusCode: 503, message: 'Unable to fetch events at this time. Please try again.' })
-      })
+  async asyncData({ $axios, error }) {
+    try {
+      const { data } = await $axios.get('http://localhost:3001/events')
+      return {
+        events: data
+      }
+    } catch (e) {
+      error({ statusCode: 503, message: 'Unable to fetch events at this time. Please try again.' })
+    }
   }
 }
 </script>
